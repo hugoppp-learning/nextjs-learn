99371d1e2aacc533d2eb4024dbf4e73364fbe702
Hugo Protsch <hugo@protsch.dev>
2025-04-13 17:53:58 +0200

    Ch3

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+﻿import {Inter, Lusitana} from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
